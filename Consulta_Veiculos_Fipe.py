0e3d459c033a7ebc42acbc40d6d9f0f755921235
Eder Assis <109834675+assis1983@users.noreply.github.com>
2022-11-24 15:03:44 -0300

    Inserindo novo arquivo

---
+++ Consulta_Veiculos_Fipe.py
@@ -0,0 +1,42 @@
+import requests
+ 
+
+def get_veiculos(ID_MARCASELECIONADA):
+    url = f'https://parallelum.com.br/fipe/api/v1/carros/marcas/{ID_MARCASELECIONADA}/modelos'
+    headers = {'user-agent': 'MyStudyApp'}
+    
+    resposta = requests.get(url, headers=headers)
+    if resposta.status_code != 200: 
+        print('Houve um erro na requisição')
+    resposta_json = resposta.json()
+    return resposta_json['modelos']
+
+
+class Fipe():
+    def __init__(self, ID_MARCASELECIONADA):
+        self.ID_MARCASELECIONADA = ID_MARCASELECIONADA
+        self.indice = 0
+        self.modelos = []
+    
+    def __iter__(self):
+        self.modelos = get_veiculos(self.ID_MARCASELECIONADA)
+        return self 
+
+    def __next__(self):
+        if self.indice >= len(self.modelos):
+            raise StopIteration
+        else:
+            modelo = self.modelos[self.indice]
+            self.indice += 1
+            return modelo
+
+
+if __name__ == '__main__':
+    ID_MARCASELECIONADA = 1
+    lista_fipe = Fipe(ID_MARCASELECIONADA)   
+    for veiculo in lista_fipe:
+        print(veiculo['codigo'])
+        print(veiculo['nome'])
+        print('_________________')
+   
+
