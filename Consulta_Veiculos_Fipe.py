f597528a8574bb35a8912e1501e938d37d6ffbca
Eder Assis <109834675+assis1983@users.noreply.github.com>
2022-11-27 08:45:34 -0300

    Atualização

--- Consulta_Veiculos_Fipe.py
+++ Consulta_Veiculos_Fipe.py
@@ -18,8 +18,8 @@ def lista_marcas():
         print('*' * 30) 
         
 
-def get_veiculos(id_marcaseleciconada):
-    url = f'https://parallelum.com.br/fipe/api/v1/carros/marcas/{id_marcaseleciconada}/modelos'
+def get_veiculos(id_marcaselecionada):
+    url = f'https://parallelum.com.br/fipe/api/v1/carros/marcas/{id_marcaselecionada}/modelos'
     headers = {'user-agent': 'MyStudyApp'}
     
     resposta = requests.get(url, headers=headers)
@@ -28,15 +28,14 @@ def get_veiculos(id_marcaseleciconada):
     resposta_json = resposta.json()
     return resposta_json['modelos']
 
-
 class Fipe():
-    def __init__(self, id_marcaseleciconada):
-        self.id_marcaseleciconada = id_marcaseleciconada
+    def __init__(self, id_marcaselecionada):
+        self.id_marcaselecionada = id_marcaselecionada
         self.indice = 0
         self.modelos = []
-    
+           
     def __iter__(self):
-        self.modelos = get_veiculos(self.id_marcaseleciconada)
+        self.modelos = get_veiculos(self.id_marcaselecionada)
         return self 
 
     def __next__(self):
@@ -52,12 +51,13 @@ if __name__ == '__main__':
     
     lista_marcas()
     
-    id_marcaseleciconada = int(input('DIGITE O CÓDIGO DA MARCA PARA COSULTAR OS MODELOS: '))
+    id_marcaselecionada = int(input('DIGITE O CÓDIGO DA MARCA PARA COSULTAR OS MODELOS: '))
     os.system('cls')   
-    lista_fipe = Fipe(id_marcaseleciconada)   
+    lista_fipe = Fipe(id_marcaselecionada)   
     for veiculo in lista_fipe:
         print(f'Código do Veículo: {veiculo["codigo"]}')
         print(f'Nome do Veículo: {veiculo["nome"]}')
-        
+
+    
               
   
