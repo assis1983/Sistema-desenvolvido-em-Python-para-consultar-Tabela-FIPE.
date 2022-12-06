b4d4080d1aef0ee60147d9910e64ea423d5d7687
Eder Assis <109834675+assis1983@users.noreply.github.com>
2022-12-06 16:27:35 -0300

    Código atualizado

--- Consulta_Veiculos_Fipe.py
+++ Consulta_Veiculos_Fipe.py
@@ -1,78 +1,72 @@
 import requests
-import os
 
-def lista_marcas():
-
-    url = f'https://parallelum.com.br/fipe/api/v1/carros/marcas'
-    headers = {'user-agent': 'MyStudyApp'}
-
-    resposta = requests.get(url, headers=headers)
-    
-    resposta_json = resposta.json()
-    lista = resposta_json
-    itlista = iter(lista)
-    
-    for marca in itlista:
-        print(f'Código: {marca["codigo"].capitalize()}')
-        print(f'Marca: {marca["nome"].capitalize()}')
-
-def get_veiculos(id_marcaselecionada):
-    url = f'https://parallelum.com.br/fipe/api/v1/carros/marcas/{id_marcaselecionada}/modelos'
+def get_veiculos():
+    url = f'http://parallelum.com.br/fipe/api/v2/carros/brands'
     headers = {'user-agent': 'MyStudyApp'}
     
     resposta = requests.get(url, headers=headers)
-    if resposta.status_code != 200: 
-        print('Houve um erro na requisição')
-    resposta_json = resposta.json()
-    return resposta_json['modelos']
-
-def lista_anos(id_marcaselecionada, id_anos):
-
-    url = f'https://parallelum.com.br/fipe/api/v1/carros/marcas/{id_marcaselecionada}/modelos/{id_anos}/anos'
-    headers = {'user-agent': 'MyStudyApp'}
-
-    resposta = requests.get(url, headers=headers)
-    
     resposta_json = resposta.json()
     lista = resposta_json
     itlista = iter(lista)
 
     for resultado in itlista:
-        print(f'Ano do Veículo: {resultado["nome"].capitalize()}')
-        print(f'Código do modelo: {resultado["codigo"].capitalize()}')
-        
+        print(f'Marcas: {resultado["name"]}')
+        print(f'Código da Marca {resultado["code"]}')
+        print('*'*30)
 
-class Fipe():
-    def __init__(self, id_marcaselecionada):
-        self.id_marcaselecionada = id_marcaselecionada
-        self.indice = 0
-        self.modelos = []
-           
-    def __iter__(self):
-        self.modelos = get_veiculos(self.id_marcaselecionada)
-        return self 
+def modelos_veiculos(brand_id):
+    url = (f'http://parallelum.com.br/fipe/api/v2/carros/brands/{brand_id}/models')
 
-    def __next__(self):
-        if self.indice >= len(self.modelos):
-            raise StopIteration
-        else:
-            modelo = self.modelos[self.indice]
-            self.indice += 1
-            return modelo
+    headers = {'user-agent': 'MyStudyApp'}
+    resposta = requests.get(url, headers=headers)
+    resposta_json = resposta.json()
+    lista = resposta_json
+    itlista = iter(lista)
 
-if __name__ == '__main__':
+    for resultado in itlista:
+        print(f'Modelo: {resultado["name"]}')
+        print(f'Código do Modelo {resultado["code"]}')
+
+def anos_modelo(id_marca, id_ano):
+    url = f'http://parallelum.com.br/fipe/api/v2/carros/brands/{id_marca}/models/{id_ano}/years'
+
+    headers = {'user-agent': 'MyStudyApp'}
+    resposta = requests.get(url, headers=headers)
+    resposta_json = resposta.json()
+    lista = resposta_json
+    itlista = iter(lista)
+
+    for resultado in itlista:
+        print(f'Modelo: {resultado["name"]}')
+        print(f'Código do Modelo {resultado["code"]}')
     
-    lista_marcas()
+def info_veiculos(marca, modelo, valor):
+    url = f'https://parallelum.com.br/fipe/api/v1/carros/marcas/{marca}/modelos/{modelo}/anos/{valor}'
+    headers = {'user-agent': 'MyStudyApp'}
     
-    id_marcaselecionada = int(input('DIGITE O CÓDIGO DA MARCA PARA COSULTAR OS MODELOS: '))
-    tipo = id_marcaselecionada
-    os.system('cls')   
-    lista_fipe = Fipe(id_marcaselecionada)   
-    for veiculo in lista_fipe:
-        print(f'Código do Veículo: {veiculo["codigo"]}')
-        print(f'Nome do Veículo: {veiculo["nome"]}')
-    id_anos = int(input('DIgite o codigo: '))
-    lista_anos(tipo, id_anos)
+    resposta = requests.get(url, headers=headers)
+    resposta_json = resposta.json()
+    lista = resposta_json
+    print(lista)
+
+if __name__== '__main__':
+    get_veiculos()
+    
+    marca = int(input('Digite o código da marca: '))
+    brand_id = marca
+    
+    modelos_veiculos(brand_id)
+    modelo = int(input('Digite o código do modelo: '))
+    id_marca = marca
+    id_ano = modelo
+    anos_modelo(id_marca, id_ano)
+    valor = str(input('Digite o código do modelo para consutar o valor: '))
+    
+    info_veiculos(marca, modelo, valor)
+
+    
+    
+    
     
     
     
